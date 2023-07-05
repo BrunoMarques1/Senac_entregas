74b5759fc4f7e49b5966b3db495ec93c5f2fd934
Bruno Marques <127341401+BrunoMarques1@users.noreply.github.com>
2023-07-05 09:35:41 -0300

    Add files via upload

---
+++ AT_Pilha/a.py
@@ -0,0 +1,13 @@
+from Livro import Livro
+from Pilha import Pilha
+
+pilha = Pilha()
+
+
+pilha.imprimir()
+#print("--------------")
+pilha.remover()
+#pilha.imprimir()
+#print("--------------")
+#pilha.remover()
+#pilha.imprimir()
